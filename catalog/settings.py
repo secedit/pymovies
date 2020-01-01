2d5b384f0028feaade7d5e089a95cadfe8b3ce44
secedit <48352716+secedit@users.noreply.github.com>
2020-01-01 17:05:04 +0100

    my firtsdjango app4

--- catalog/settings.py
+++ catalog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uh1cb5jqe$_@#los(xkn*vj%ek1ql7xll_nqu(f%8l4a6p^kj#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
